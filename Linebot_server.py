739e93f58800ff96d7e6d7b1c4d044f904b10ba6
baljeet0531 <e14056300@gs.ncku.edu.tw>
2021-06-20 19:37:02 +0800

    finish part web and merge code with master

--- Linebot_server.py
+++ Linebot_server.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
-import configparser
-import json
-import os
-import requests
-import time
-from apscheduler.schedulers.background import BackgroundScheduler
-import app_recommend
-from bs4 import BeautifulSoup
-from threading import Timer
-
 import Linebot_function as lf  # import自己寫的linebot功能
+from threading import Timer
+from bs4 import BeautifulSoup
+import app_recommend
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+import datetime
+import requests
+import os
+import json
+import configparser
+from linebot.models import *
+from linebot.exceptions import InvalidSignatureError
+from linebot import LineBotApi, WebhookHandler
+from flask import Flask, request, abort, send_file
+
 # import XXXX 時如果有error，請google python install XXXX，通常在terminal下 pip install XXXX 指令就可以安裝了
 #
 # line bot api reference: https://github.com/line/line-bot-sdk-python
@@ -70,32 +71,37 @@ def callback():
 
     return 'OK'
 
-#收到語音訊息__記帳
+# 收到語音訊息__記帳
+
+
 @handler.add(MessageEvent, message=AudioMessage)
 def handle_message(event):
     print(event)
-    a=lf.return_alert_data()[event.source.user_id]["audio"]
+    a = lf.return_alert_data()[event.source.user_id]["audio"]
     print(a)
     if event.source.type == 'user':
-        a=int(a)+1
-        lf.enter_alert_audio_data(event.source.user_id,str(a))
+        a = int(a)+1
+        lf.enter_alert_audio_data(event.source.user_id, str(a))
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text="記得記帳"))
 
 
-
 # 收到message event
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print(event)  # 看event長怎樣
     text = event.message.text
 
     if text == "記帳推薦":
+
         muilt_reply = []
         muilt_reply.append(TextSendMessage
                            (text="以下為您的ID以及推薦你適合記帳程式的連結。\n進入連結後請在第一題填入我們提供的ID進行，謝謝！"))
         muilt_reply.append(TextSendMessage(text=event.source.user_id))
-        muilt_reply.append(TextSendMessage(text='https://forms.gle/9i3bmXM6QXJv3gpV8'))
+        muilt_reply.append(TextSendMessage(
+            text='https://forms.gle/9i3bmXM6QXJv3gpV8'))
         response = line_bot_api.reply_message(
             event.reply_token, muilt_reply)
 
@@ -103,7 +109,7 @@ def handle_message(event):
         with open('questionnaire_data.json', 'r', encoding='utf-8') as object:
             q_d = json.load(object)
 
-        if q_d[event.source.user_id] == event.source.user_id: ##這句有問題
+        if q_d[event.source.user_id] == event.source.user_id:  # 這句有問題
             app = app_recommend.systems(event.source.user_id)
             response = line_bot_api.reply_message(
                 event.reply_token, TextSendMessage(
@@ -117,13 +123,15 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             FlexSendMessage(
-                     alt_text="記帳提醒",
-                     contents=flex_message)
-            )
+                alt_text="記帳提醒",
+                contents=flex_message)
+        )
         lf.alert_data(event.source.user_id)
 
     elif text == "網址":  # liff網址
         response = "https://liff.line.me/1656056998-RP6bYLXr"
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=response))
 
     elif text == "我又沒有唸書":
         # send image_message
@@ -167,26 +175,27 @@ def handle_message(event):
 
         elif event.source.type == 'user':  # 個人功能
             response = "個人功能"
-    elif text=="已經記了":
-        lf.enter_alert_audio_data(event.source.user_id,"0")
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text=response))
+
+    elif text == "已經記了":
+        lf.enter_alert_audio_data(event.source.user_id, "0")
     else:
         return
 
-        # 回覆文字訊息
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text= response))
-
-
 # 收到Postback event
+
+
 @ handler.add(PostbackEvent)
 def handle_postback(event):
     print(event)
-    postback=event.postback.data
-    time=event.postback.params
+    postback = event.postback.data
+    time = event.postback.params
 
-    if postback=="time":
-        reply="設定成功!將於每日"+time["time"]+"提醒你記帳!"
-        lf.enter_alert_time_data(user_id=event.source.user_id,tm=time["time"],)
+    if postback == "time":
+        reply = "設定成功!將於每日"+time["time"]+"提醒你記帳!"
+        lf.enter_alert_time_data(
+            user_id=event.source.user_id, tm=time["time"],)
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text=reply))
 
@@ -202,19 +211,23 @@ def handle_join(event):
             preview_image_url=ngrok_url + "/image/hi.jpg"
         )
     )
+
+
 def alert():
     while lf.get_alert_time_user():
-        alert_id,audio_id = lf.get_alert_time_user()
+
+        alert_id, audio_id = lf.get_alert_time_user()
         # print(user_id)
 
         try:
             for i in alert_id:
                 line_bot_api.push_message(i, TextSendMessage(text='記得每天記帳呦!'))
-                #break
-            #break
+                # break
+            # break
             for i in audio_id:
 
-                mess="你還有{}筆語音還沒紀錄".format(lf.return_alert_data()[i]["audio"])
+                mess = "你還有{}筆語音還沒紀錄".format(
+                    lf.return_alert_data()[i]["audio"])
                 line_bot_api.push_message(i, TemplateSendMessage(
                                           alt_text='Confirm template',
                                           template=ConfirmTemplate(
@@ -230,21 +243,44 @@ def alert():
                                                   )
                                               ]
                                           )
-                                      ))
+                                          ))
                 break
             break
         except:
             pass
 
 
-
-sched = BackgroundScheduler()
-sched.add_job(alert,'cron',second=59)
-sched.start()
-
-
+if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+    sched = BackgroundScheduler()
+    sched.add_job(alert, 'cron', second=59)
+    sched.start()
 
 
+def check():
+    while lf.get_audio_user():
+        now = time.ctime().split(" ")[3][:5]
+        if now == "22:32":
+            user_id = lf.get_audio_user()
+            template_message = lf.setting_check_message()
+            for i in user_id:
+                line_bot_api.push_message(i,
+                                          TemplateSendMessage(
+                                              alt_text='Confirm template',
+                                              template=ConfirmTemplate(
+                                                  text='Are you sure?',
+                                                  actions=[
+                                                      MessageAction(
+                                                          label='已經記了',
+                                                          text='yes'
+                                                      ),
+                                                      MessageAction(
+                                                          label='等等再說',
+                                                          text='no'
+                                                      )
+                                                  ]
+                                              )
+                                          ))
+    time.sleep(59)
 
 
 if __name__ == "__main__":
--- webpage/index.js
+++ webpage/index.js
@@ -18,13 +18,123 @@ $('#detail-btn').click(function () {
     $('#main-page,#detail-page').stop();
     $('#detail-page').css("display", "block");
     $('#detail-page').animate({ 'left': '0vw' });
-    $('#main-page').animate({ 'right': '100vw' }, 500, function () { $('#main-page').css('display', 'none'); })
+    $('#main-page').animate({ 'left': '-100vw' }, 500, function () { $('#main-page').css('display', 'none'); })
 })
 
 $('.back-btn').click(function () {
     $('#main-page,#detail-page').stop();
     $('#main-page').css("display", "block");
-    $('#main-page').animate({ 'right': '0vw' });
+    $('#main-page').animate({ 'left': '0vw' });
     $('#detail-page').animate({ 'left': '100vw' }, 500, function () { $('#detail-page').css('display', 'none'); })
 })
+
+$('#add-btn').click(function () {
+    if ($('#main-page').css("display") == "block") {
+        $('#add-page').css({ 'left': '100vw', "display": "block" });
+        $('#add-page').animate({ 'left': '0vw' });
+        $('#main-page').animate({ 'left': '-100vw' }, 500, function () {
+            $('#main-page').css('left', '0vw');
+            $('#detail-page').css({ 'left': '100vw', "display": "none" });
+        })
+    }
+    else if ($('#detail-page').css("display") == "block") {
+        $('#add-page').fadeIn(500, function () {
+            $('#main-page').css('left', '0vw');
+            $('#detail-page').css({ 'left': '100vw', "display": "none" });
+        });
+    }
+})
+
+$('#close-btn').click(function () {
+    $('#main-page').css('display', 'block');
+    $('#add-page').fadeOut(500);
+})
+
+$('#next-btn').click(function () {
+    if ($('#category-flex div').hasClass('select-cate') && $('#add-items input').val() && $('#add-price input').val()) {
+        $('#add-content,#share-content').stop();
+        $('#share-content').css({ 'left': '100vw', "display": "block" });
+        $('#share-content').animate({ 'left': '0vw' });
+        $('#add-content').animate({ 'left': '-100vw' }, 500, function () { $('#add-content').css('display', 'none'); })
+        $('#save-btn-inact, #add-pg-back-btn').fadeIn(500);
+        $('#next-btn').fadeOut(500);
+
+        calculateShare();
+    }
+})
+
+$('#add-pg-back-btn').click(function () {
+    $('#add-content,#share-content').stop();
+    $('#add-content').css("display", "block");
+    $('#add-content').animate({ 'left': '0vw' });
+    $('#share-content').animate({ 'left': '100vw' }, 500, function () { $('#share-content').css('display', 'none'); })
+    $('#save-btn-inact, #add-pg-back-btn').fadeOut(500);
+    $('#next-btn').fadeIn(500);
+})
+
+
+$('#category-flex div').click(function () {
+    if ($(this).attr('class') == "") {
+        $('#category-flex div').attr('class', "");
+        $('#category-flex div').css('border-color', '#FFFFFF');
+        $(this).attr('class', 'select-cate');
+        $(this).css('border-color', '#515472');
+    }
+    else if ($(this).attr('class') == 'select-cate') {
+        $(this).attr('class', "");
+        $(this).css('border-color', '#FFFFFF');
+    }
+})
+
+$('#share-user-flex .user-name-container').click(function () {
+    var userName = $(this).children('p').html();
+    if (!$(this).hasClass('selected')) {
+        $(this).toggleClass('selected');
+        $(this).children('p').css('color', '#000000DE');
+        $(this).css('background-color', '#EADAA6');
+
+        $(`<div class=\"edit-share-user\" id=${userName}>`).appendTo('#edit-share-user-container');
+        var txt = ""
+
+        txt += "<div id=\"edit-share-user-name\">";
+        txt += "<div class=\"user-name-container\">";
+        txt += `<p class=\"user-name\">${userName}</p>`;
+        txt += "</div>";
+        txt += "</div>";
+        txt += "<div id=\"edit-share-user-price\">";
+        txt += "<input type=\"number\" min=\"1\" oninput=\"validity.valid||(value='');\">";
+        txt += "<p class=\"p-twd\">twd</p>";
+        txt += "</div>";
+        txt += "</div>"
+
+        $(`#${userName}`).html(txt);
+    }
+    else if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+        $(this).children('p').css('color', '#7C7A7ADE');
+        $(this).css('background-color', '#EFEEED');
+        $(`#edit-share-user-container #${userName}`).remove();
+    }
+
+    calculateShare();
+})
+
+function calculateShare() {
+    var totalPrice = $('#add-price input').val();
+    var shareUserNum = $('#edit-share-user-container').children().length;
+    var avgShare = parseInt(totalPrice / shareUserNum);
+    var extraShare = totalPrice % shareUserNum;
+    for (i = 1; i <= shareUserNum; i++) {
+        if (i <= extraShare) {
+            $(`#edit-share-user-container .edit-share-user:nth-child(${i}) input`).val(avgShare + 1);
+        }
+        else {
+            $(`#edit-share-user-container .edit-share-user:nth-child(${i}) input`).val(avgShare);
+        }
+    }
+}
+
+
+
+
 
