f80db7a2aaf8cf844223dacd0e30231e24237df6
baljeet0531 <e14056300@gs.ncku.edu.tw>
2021-06-24 21:34:13 +0800

    fix reply text

--- Linebot_server.py
+++ Linebot_server.py
@@ -364,8 +364,6 @@ def handle_message(event):
         elif text == "現在去記":
             return
         else:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='我看不懂QQ'))
             return
 
     if event.source.type == 'group':  # 群組功能
@@ -391,8 +389,6 @@ def handle_message(event):
             )
             return
         else:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='我看不懂QQ'))
             return
 
 
